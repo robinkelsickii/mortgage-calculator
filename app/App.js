5818b97e89e9a30e30962a28e79fd1e72760139b
robinkii <robinkelsickii@gmail.com>
2025-01-06 14:06:23 -0500

    Refactor App.js: remove padding and set margin to zero for layout adjustments

--- app/App.js
+++ app/App.js
@@ -19,6 +19,7 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
-    padding: 20,
+    padding: 0,
+    margin: 0,
   },
 });
